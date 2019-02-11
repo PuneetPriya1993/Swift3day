7aecc1bac034a32610c04f33eb17bb7cc0fd2ed0
MacStudent <macstudent@126-iMac06.local>
2019-02-11 18:56:27 -0500

    functions

--- Swift3day/main.swift
+++ Swift3day/main.swift
@@ -10,4 +10,46 @@ import Foundation
 
 print("Hello, World!")
 
-print("github")
+print("-------------------functions---------------------")
+
+
+func hello()
+{
+    print("MADT PROGRAM W2019")
+}
+
+hello()
+
+
+func sum(a: Int, b: Int)
+{
+    let c = a+b;
+    print("sum of a and b: ", c)
+}
+sum(a: 10, b: 20)
+//sum(b: 100, a: 50) error bcos a must be at first place
+
+print("--------------------------------function overloading----------------------------------------")
+func sum(a: Float, b: Float)
+{
+    let d = a+b;
+    print("Sum of float: ", d)
+}
+
+sum(a: 10.5, b: 20.10)
+
+func sum(of a: Float, and b: Float)
+{
+    let d = a+b;
+    print("Sum of float with labels: ", d)
+}
+
+sum(of: 10.5, and: 20.10)
+
+func sub(_ a: Float, _ b: Float)
+{
+    let d = a-b;
+    print("Subtraction of float without labels: ", d)
+}
+
+sub( 10.5, 20.10)
