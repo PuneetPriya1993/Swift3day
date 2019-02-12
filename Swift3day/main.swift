4e8a7a69ab35cb129e5bbeaa9720005b04a25eeb
MacStudent <macstudent@126-iMac06.local>
2019-02-11 19:11:46 -0500

    returning values from functions

--- Swift3day/main.swift
+++ Swift3day/main.swift
@@ -53,3 +53,38 @@ func sub(_ a: Float, _ b: Float)
 }
 
 sub( 10.5, 20.10)
+
+print("------------Returning value from function------------------------")
+func greet(name: String) -> String
+{
+    let s = "Welcome, \(name)"
+    return s
+}
+
+let s = greet(name: "Priya Aggarwal")
+print(s)
+
+
+func exchange(a: Int, b: Int) -> (Int, Int)
+{
+    return(b,a)
+}
+
+var x = 100
+var y = 300
+print("without swapping: ", x,y)
+(x,y) = exchange(a: x, b: y)
+print("With swapping: ",x,y)
+
+
+func si(amount: Float = 100, rate: Float, year: Float = 2) -> Float
+{
+    return(amount * rate * year)/100.0
+}
+
+print("Simple Interest: ",si(amount: 1000, rate: 2.5, year: 10))
+
+print("Simple Interest with default value of year: ",si(amount: 1000, rate: 2.5))
+
+
+print("Simple Interest with deafulat values of amounts and year: ",si(rate: 2.0))
